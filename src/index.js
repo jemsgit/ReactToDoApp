b80f4da08618e8b24aa8652b0ef3fa4281a594f9
Evgenii J <jemguns@gmail.com>
2017-09-07 00:40:39 +0400

    working webpack cnfiguration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-document.getElementById('root').innerHTML = 'Привет, я готов.';
+import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import App from './containers/App'
+
+
+render(
+  <App />,
+  document.getElementById('root')
+)
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 var path = require('path');
-var webpack = require('webpack')
+var webpack = require('webpack');
+var fs = require('fs');
+var babelrc = fs.readFileSync('./.babelrc');
+
+var babelrcObject = {};
+
+try {
+  babelrcObject = JSON.parse(babelrc);
+} catch (err) {
+  console.error('==>     ERROR: Error parsing your .babelrc.');
+  console.error(err);
+}
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
+    'webpack-hot-middleware/client',
+    'babel-polyfill',
     './src/index'
   ],
   output: {
@@ -13,6 +26,35 @@ module.exports = {
   },
   plugins: [
     new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.NoErrorsPlugin()
-  ]
+    new webpack.NoErrorsPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    rules: [ //добавили babel-loader
+        {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: [
+              {
+                loader: 'react-hot-loader'
+              },
+              {
+                loader: 'babel-loader',
+                options: babelrcObject
+              },
+            ]
+        },
+        {
+            test: /\.jsx$/,
+            enforce: "pre",
+            loader: "eslint-loader",
+            include: [
+                path.resolve(__dirname, "src"),
+            ]
+          }
+    ]
+  },
+  resolve: {
+    extensions: ['.json', '.js', '.jsx'],
+  }
 }
